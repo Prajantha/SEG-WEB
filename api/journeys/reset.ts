5086aeedb0800cd17d89c5243ec36904c5e34ee9
Prajantha P.A. <169165090+Prajantha@users.noreply.github.com>
2026-09-16 01:26:47 +0530

    Update reset.ts

--- api/journeys/reset.ts
+++ api/journeys/reset.ts
@@ -1,4 +1,4 @@
-import { resetJourneys } from '../lib/storage';
+import { resetJourneys } from '../lib/storage.js';
 
 export default async function handler(req: any, res: any) {
   res.setHeader('Access-Control-Allow-Origin', '*');
